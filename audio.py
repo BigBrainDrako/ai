eb2effeb278ff1d0cb59efad09a8ee045f28e1e6
BigBrainDrako <132248090+BigBrainDrako@users.noreply.github.com>
2023-05-21 15:38:43 +0530

    Update audio.py

--- audio.py
+++ audio.py
@@ -5,8 +5,9 @@ from collections import deque
 
 st.title("Text to Speech App powered by Gradio API hosted on 🤗")
 
-# Initialize a deque to store the last 5 input texts
-last_five_inputs = deque(maxlen=5)
+# Initialize the last_five_inputs list in the st.session_state dictionary
+if 'last_five_inputs' not in st.session_state:
+    st.session_state.last_five_inputs = deque(maxlen=5)
 
 inp_text = st.text_input("Enter your text here.....")
 
@@ -21,8 +22,8 @@ if button:
             inp_text,
         ]}).json()
 
-        # Append the input text to the deque
-        last_five_inputs.append(inp_text)
+        # Append the input text to the last_five_inputs list in st.session_state
+        st.session_state.last_five_inputs.append(inp_text)
 
         md = f"""
             <audio controls>
@@ -32,7 +33,7 @@ if button:
         st.markdown(md, unsafe_allow_html=True)
 
 # Show the last 5 input texts
-if last_five_inputs:
+if st.session_state.last_five_inputs:
     st.write("Last 5 input texts:")
-    for i, inp in enumerate(last_five_inputs):
+    for i, inp in enumerate(st.session_state.last_five_inputs):
         st.write(f"{i+1}. {inp}")
